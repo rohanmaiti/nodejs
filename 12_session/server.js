fbfc8e2795798f023de10300e43a2c8521d1e67a
rohanmaiti <maitirohan2019@gmail.com>
2025-01-25 14:03:31 +0530

    session completed

--- 12_session/server.js
+++ 12_session/server.js
@@ -1,25 +1,92 @@
 const session = require("express-session");
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 app.listen(4000, (err)=>{
     err ? console.log("error", err) 
     : console.log("server started at port 4000");
 })
-
+// json parsing 
+app.use(express.json());
+app.use(express.urlencoded());
 // initialising session 
 app.use(session({
     secret : "secret",
     resave : false,
-    saveUninitialized:false
+    saveUninitialized:false,
 }))
-/**
- * *saveUninitialized:
- *  If set to false, it prevents uninitialized sessions (sessions with no data) from being saved to the store.
- * *resave: 
- *  If set to false, the session will not be saved back to the server if it hasn’t been modified during the   request.
- * *secret: 
- * A string used to sign the session ID cookie for security. It ensures that session data can't be tampered with.
- * 
- */
+
+// initialising mongoose 
+const uri = "mongodb+srv://rohan:rohan123@cluster0.qzw60o2.mongodb.net/AuthSession?retryWrites=true&w=majority&appName=Cluster0";
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        unique:true
+    },
+    password:String
+})
+const User = mongoose.model("user",userSchema);
+mongoose.connect(uri).then(()=>{
+    console.log("mongoose connected");
+}).catch((err)=>{
+    console.log("Error connecting to mongoose", err.message);
+})
+
+// creating middlewire for authentication
+function authUser(req,res,next){
+if(req.session.user){
+next();    
+}
+else{
+    res.redirect("/");
+}
+}
+
+// prevent user to get login page if already login
+function checkLogin(req,res,next){
+    if(req.session && req.session.user){
+        res.redirect("/home")
+    }
+    else{
+        next();
+    }
+}
 
 
+app.get("/",checkLogin,(req,res)=>{
+    res.sendFile("login.html",{root: "./html"})
+})
+
+app.post("/login",async (req,res)=>{
+    console.log(req.body);
+    const {email, password} = req.body;
+    const user = await User.findOne({email:email, password:password});
+    if(!user){ // user not found 
+        res.redirect("/");
+    }
+    else{
+        // initialising session
+        // By default, express-session creates a session that lasts until the client’s browser is closed. 
+        req.session.user = req.body;
+        res.redirect("/dashboard");
+    }
+})
+
+app.get("/dashboard",authUser,(req,res)=>{
+    res.sendFile("dashboard.html",{root:"./html"});
+})
+app.get("/home",authUser,(req,res)=>{
+    res.sendFile("home.html",{root:"./html"});
+})
+app.get("/about",authUser, (req,res)=>{
+    res.sendFile("about.html",{root:"./html"});
+})
+
+
+// for logout as it is comming from fetch , then have to handle in home.html
+app.post("/logout",(req,res)=>{
+    console.log(req.session);
+    req.session.destroy();
+    console.log(req.session);
+    res.redirect("/");
+})
