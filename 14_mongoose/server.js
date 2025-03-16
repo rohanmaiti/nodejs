783a680f5e98458194bbfd5143e8c5a8f6e2c292
rohanmaiti <maitirohan2019@gmail.com>
2025-03-16 21:47:23 +0530

    mongoose updated in node js

--- 14_mongoose/server.js
+++ 14_mongoose/server.js
@@ -4,7 +4,10 @@ const app = express();
 app.listen(4000,(req,res)=>{
     console.log("server started at 4000");
 })
+app.use(express.urlencoded());
+app.use(express.json());
 
+// creating schema and model
 const userShcema = new mongoose.Schema({
     name:String,
     age:{
@@ -21,14 +24,32 @@ const userShcema = new mongoose.Schema({
     }
 
 })
-
-const user = mongoose.model("user", userShcema);
+const User = mongoose.model("user", userShcema);
 
 // connection 
-mongoose.connect("mongodb://127.0.0.1:27017/demoDatabase-01")
+mongoose.connect("mongodb+srv://rohan:rohan123@cluster0.qzw60o2.mongodb.net/mongooseDataBase")
 .then(()=>{
     console.log("mongoose connected");
 })
 .catch((err)=>{
     console.log("mongoose error",err.message);
+})
+
+
+// CRUD operations
+// ** Create
+app.post("/signup",async (req,res)=>{
+    
+    const {name , age, address, email} = req.body;
+    console.log(req.body);
+    try {
+        // type-01
+        const user =await new User({name:name, age:age, address:address, email:email});
+        await user.save();
+        console.log(user);
+    } catch (error) {
+        
+    }
+
+    res.send("ok");
 })
