681f1357a6f9d4d73cff8af28093d1b9b1c90471
rohanmaiti <maitirohan2019@gmail.com>
2025-07-05 09:44:13 +0530

    updated:

--- 14_mongoose/server.js
+++ 14_mongoose/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { string } = require("zod/v4");
 const app = express();
 app.listen(4000,(req,res)=>{
     console.log("server started at 4000");
@@ -21,6 +22,18 @@ const UserShcema = new mongoose.Schema({
     email:{
         type:String,
         unique: true
+    }
+
+})
+
+const CourseSchema = new Mongoose.Schema({
+    course_id: {
+        type:string
+    },
+    title : string,
+    price: string,
+    owner_id: {
+        type: Mongoose.Types.ObjectType 
     }
 
 })
